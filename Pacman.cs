694da3c05ee0870465d99bd9b1e8c0e8dff72c97
knguyen44 <71211696+knguyen44@users.noreply.github.com>
2022-09-27 10:49:45 -0500

    Initial commit

---
+++ Pacman.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mono_InterestingMovementPacman
+{
+    public enum PacManState { Spawning, Still, Chomping, SuperPacMan, Dying, Dead }
+
+    public class PacMan 
+    {
+
+        protected PacManState _state;
+        public PacManState State
+        {
+            get { return _state; }
+            set
+            {
+                if (_state != value)
+                {
+                    this.Log(string.Format("{0} was: {1} now {2}", this.ToString(), _state, value));
+
+                    _state = value;
+                }
+            }
+        }
+
+        public PacMan()
+        {
+            //Set default state will call notify so make sure this.Ghosts is intitialized first
+            this.State = PacManState.Still;
+        }
+
+        public void PowerUp()
+        {
+            this.State = PacManState.SuperPacMan;
+        }
+
+        public void PowerDown()
+        {
+            this.State = PacManState.Still;
+        }
+
+        public void Move()
+        {
+            this.State = PacManState.Chomping;
+        }
+
+        public void Kill()
+        {
+            this.State = PacManState.Dying;
+        }
+
+        public void Spawn()
+        {
+            this.State = PacManState.Spawning;
+        }
+
+        //Extra method for logging state change
+        public virtual void Log(string s)
+        {
+            //nothing
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Mono_InterestingMovementPacman.Game1();
+game.Run();
